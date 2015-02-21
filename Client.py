f7b11ae4a515eeddb4b3e7871cec284ac792bdd7
gordoncheung <itsagordon@gmail.com>
2015-02-21 15:51:17 -0800

    Update Client.py
    Updated Client to account for Broadcasted messages from Server.

--- Client.py
+++ Client.py
@@ -1,68 +1,105 @@
-import socket
+import socketserver
 import sys
 import struct
+import threading
 from Crypto.PublicKey import RSA
 import rsa
-#myKey = RSA.generate(1024)
-#pubKey = myKey.publickey()
-#print("myKey: ",myKey.exportKey())
-#print("pubKey: ",pubKey.exportKey())
-myKey = rsa.newkeys(1024) #Tuple of Private and Public Key
-pubKey = myKey[0]
-privKey = myKey[1]
-pubKeyInBytes = pubKey.save_pkcs1(format='PEM')#This is ready to be sent
+from multiprocessing import Process
+import socket
 
-'''
-The link to the code that I used for this is in the server_python_tcp.py file
-This code mostly consists of sends and recv's. Just some logic added in
-to determine what to do with certain commands
-'''
-if len(sys.argv) != 3:
-    print("ERROR: Invalid number of args. Terminating.", file=sys.stderr)
-    sys.exit(0)
-HOST, PORT = sys.argv[1], int(sys.argv[2])
+#https://docs.python.org/3.4/library/socketserver.html
 
-if (PORT > 65535 or PORT < 1024):
-    print("ERROR: Invalid port. Terminating.", file=sys.stderr)
-    sys.exit(0)
+clientList = []
 
-# Create a socket (SOCK_STREAM means a TCP socket)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class MyTCPHandler(socketserver.BaseRequestHandler):
 
-try:
-    # Connect to server and sendall data
-    sock.connect((HOST, PORT))
-    # Upon connection, send key to server
-    sock.sendall(struct.pack("I",len(pubKeyInBytes)))
-    sock.sendall(pubKeyInBytes)
+    def send(self, stringmessage):#Helper function to send messages, not completed, no time
+        bytemsg = bytes(stringmessage, "utf-8")
+        self.request.sendall(struct.pack("I",len(stringmessage)))
+        self.request.sendall(bytes(value,"utf-8"))
+
+    def handle(self):
+        #print("Socket Info: ",self.client_address)
+        #clientList.append(self.client_address)
+        #clientKeySize = self.request.recv(4)
+        #clientKeySize = struct.unpack("I",clientKeySize)[0]
+        #clientKey = self.request.recv(int(clientKeySize))
+        #print("Client's Key: ",clientKey)
+        
+        self.data = ""
+        while(self.data != "exit"):
+            self.size = self.request.recv(4)
+            if self.size:
+                self.size = struct.unpack("I", self.size)[0]
+                if(int(self.size) != 0):   
+                    self.data = self.request.recv(int(self.size))#.strip()
+                    self.data = str(self.data,"utf-8")
+                    print(self.data)                         
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+                
+if __name__ == "__main__":
+
+    myKey = rsa.newkeys(1024) #Tuple of Private and Public Key
+    pubKey = myKey[0]
+    privKey = myKey[1]
+    pubKeyInBytes = pubKey.save_pkcs1(format='PEM')#This key is ready to be sent
     
-    print("Connected.")
+    print("Server Ready")
+    HOST, PORT = "localhost",  0 #0 finds an arbitrary available port
+
     dictionary = {}
-    while True:
-        try:
-            command = input()
-            if command:
-                #sock.sendall(bytes(command,"utf-8"))          
-                size = len(command)
-                size32bit = struct.pack("I",size)
-                sock.sendall(size32bit)#sendalling the size of the command
-                sock.sendall(bytes(command, "utf-8"))#command to sendall
-            else:
-                receivedValSize = sock.recv(4)#Receive back size of message returned from server
-                if receivedValSize:
-                    receivedValSizeUnpack=struct.unpack("I",receivedValSize)[0]#Unpack the size packet
-                if int(receivedValSizeUnpack) >0:
-                    receivedValue = str(sock.recv(int(receivedValSizeUnpack)),"utf-8")#Final Message received from server
-                    print(receivedValue)
-            
-        except:
-            print("ERROR: Invalid packet from server. Terminating", file=sys.stderr)
-            sys.exit(0)
-except:
-    #print("ERROR: Could not connect to server. Terminating.", file=sys.stderr)
-    sys.exit(0)    
+    # Create the server, binding to localhost on port 9999
     
-finally:
-    print("End Client")
-    #sock.close()
+    print("Begin Client Interaction: \n", file = sys.stdout)
+    
+##############################################################################
+######################CLIENT##################################################
+##############################################################################    
+    
+    #Client callback function for threading
+    def client():
+           # Create a socket (SOCK_STREAM means a TCP socket)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            sock.connect(('localhost',9999))#9999 is the main server
+            # Upon connection, send key to server + info on client's Server
+            sock.sendall(struct.pack("I",len(pubKeyInBytes)))
+            sock.sendall(pubKeyInBytes)
+            sock.sendall(struct.pack("I",len(str(HOST))))
+            sock.sendall(bytes(str(HOST),"utf-8"))
+            sock.sendall(struct.pack("I",len(str(PORT))))
+            sock.sendall(bytes(str(PORT),"utf-8"))
+            
+            print("Connected.")
+            dictionary = {}
+            while True:
+                try:
+                    command = input()
+                    if command:
+                        #sock.sendall(bytes(command,"utf-8"))          
+                        size = len(command)
+                        size32bit = struct.pack("I",size)
+                        sock.sendall(size32bit)#sendalling the size of the command
+                        sock.sendall(bytes(command, "utf-8"))#command to sendall
+                    
+                except:
+                    print("ERROR: Invalid packet from server. Terminating", file=sys.stderr)
+                    sys.exit(0)
+        except:
+            #print("ERROR: Could not connect to server. Terminating.", file=sys.stderr)
+            sys.exit(0)    
+            
+        finally:
+            print("End Client")
+    
+    server = ThreadedTCPServer((HOST,PORT), MyTCPHandler)
+    HOST, PORT = server.server_address
+    c = threading.Thread(target = client)
+    s = threading.Thread(target = server.serve_forever)
+    s.start()
+    c.start()
+    
 
