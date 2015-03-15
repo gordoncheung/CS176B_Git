53ef8e0413421a501906b78cb2674fdf31c98211
gordoncheung <itsagordon@gmail.com>
2015-03-15 13:39:48 -0700

    Update Client.py
     Now correctly handles >3 clients by breaking up the json object.

--- Client.py
+++ Client.py
@@ -10,6 +10,7 @@ import socket
 import os
 import json
 import base64
+import random
 
 #https://docs.python.org/3.4/library/socketserver.html
 
@@ -58,18 +59,25 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                         clientMap.clear()
                         for key in updatedMap:
                             clientMap[key]=updatedMap[key]
-                        #print("ClientMap: ",clientMap)
+
                     elif(self.data == '00000001'):#00000001 means decrypt this segment
-                        #print("00000001 Flag. Server Decrypt")
                         size = self.request.recv(4)
                         size = struct.unpack("I", size)[0]
-                        jsonString = self.request.recv(int(size))
-                        jsonString = str(jsonString, 'utf-8')
+                        #jsonString = self.request.recv(int(size))
+                        #jsonString = str(jsonString, 'utf-8')
+                        #jsonData = json.loads(jsonString)
+                        numSegments = self.request.recv(int(size))
+                        numSegments = int(str(numSegments,'utf-8'))
+                        jsonString = ''
+                        for i in range(numSegments):
+                            size = self.request.recv(4)
+                            size = struct.unpack("I",size)[0]
+                            currString = self.request.recv(int(size))
+                            currString = str(currString,'utf-8')
+                            jsonString += currString
                         jsonData = json.loads(jsonString)
-                        #print(jsonData)
-                        #print('received json: ',jsonString)
-                        #The last TWO elements are guaranteed to be the SYMMETRIC Key
-                        #Pop these two off and handle separately
+                        
+
                         mySymIV = jsonData.pop()
                         mySymKey = jsonData.pop()
 
@@ -78,16 +86,14 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                         mySymKey = rsa.decrypt(base64.b16decode(bytes(mySymKey,'utf-8')),privKey)
                         mySymIV = base64.b16decode(mySymIV)
                         mySymKey = base64.b16decode(mySymKey)
-                        #print("SymIV & Key: ", mySymIV, mySymKey)                        
+                        
                         #In a loop, decode everything back to utf-8
                         #Now, decrypt everything using the Symmetric Key
                         for i in range(len(jsonData)):
                             jsonData[i] = base64.b16decode(jsonData[i])
                             myAES = AES.new(mySymKey, AES.MODE_CFB, mySymIV)
                             jsonData[i] = myAES.decrypt(jsonData[i])
-                            jsonData[i] = str(jsonData[i],'utf-8')
-                        
-                        
+                            jsonData[i] = str(jsonData[i],'utf-8')                       
                         
                         #Handle Destination
                         flag = jsonData.pop()
@@ -98,13 +104,32 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                             tmpSock.connect((serverData[1],int(serverData[2])))
                         else:
                             tmpSock.connect((clientMap[destKey][0],int(clientMap[destKey][1])))
-                        tmpSock.sendall(struct.pack("I",len(b'00000001')))
+
                         #Send the flag 00000001
                         #This flag indicates that the receiver needs to decrypt the message
+                        tmpSock.sendall(struct.pack("I",len(b'00000001')))
                         tmpSock.sendall(b'00000001')
-                        tmpSock.sendall(struct.pack("I",len(bytes(jsonData,'utf-8'))))
-                        tmpSock.sendall(bytes(jsonData,'utf-8'))
-                        tmpSock.shutdown(SHUT_RDWR)
+                        #tmpSock.sendall(struct.pack("I",len(bytes(jsonData,'utf-8'))))
+                        #tmpSock.sendall(bytes(jsonData,'utf-8'))
+                        
+                        segments = []
+                        currString = ''
+                        for i in range(len(jsonData)):
+                            currString += jsonData[i]
+                            if((i % 4000 == 0) and i>0):
+                                segments.append(currString)
+                                currString = ''
+                        segments.append(currString)
+                        #print('jsonData ',jsonData)
+                        #print('segments: ',segments)
+                        numSegments = len(segments)
+                        tmpSock.sendall(struct.pack("I",1))
+                        tmpSock.sendall(bytes(str(numSegments),'utf-8'))
+                        for seg in segments:
+                            tmpSock.sendall(struct.pack("I",len(seg)))
+                            tmpSock.sendall(bytes(seg,'utf-8'))
+                        
+                        tmpSock.shutdown(socket.SHUT_RDWR)
                         tmpSock.close()
                     
                     elif(self.data == '00000011'):
@@ -113,9 +138,9 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                         msgList = self.request.recv(int(listSize))
                         msgList = str(msgList,'utf-8')
                         msgList = json.loads(msgList)
-                        print("#     Votes  Message\n")
+                        print("#   Message\n")
                         for item in msgList:
-                            print(str(item[1]) + "     " + str(item[2]) + "   |  " + str(item[0]))
+                            print(str(item[1]) + " | " + str(item[0]))
                         
                     elif(self.data == '00000005'):#Receiving msg and printing
                         msgSize = self.request.recv(4)
@@ -138,19 +163,22 @@ if __name__ == "__main__":
     
     print("Server Ready")
     HOST, PORT = "localhost",  0 #0 finds an arbitrary available port
-
     clientMap = {}
     # Create the server, binding to localhost on port 9999
-    
+    if len(sys.argv) != 3:
+        print("ERROR: Invalid number of args. Terminating.")
+        sys.exit(0)
+    serverHOST, serverPORT = sys.argv[1], int(sys.argv[2])
+    if (serverPORT > 65535 or serverPORT < 1024):
+        print("ERROR: Invalid port. Terminating.", file=sys.stderr)
+        sys.exit(0)
+
     print("Begin Client Interaction: \n", file = sys.stdout)
     serverData = []
-##############################################################################
-######################CLIENT##################################################
-##############################################################################    
     
     #Client callback function for threading
     def client():
-           # Create a socket (SOCK_STREAM means a TCP socket)
+        # Create a socket (SOCK_STREAM means a TCP socket)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         def receive():
             size = sock.recv(4)
@@ -166,7 +194,8 @@ if __name__ == "__main__":
             return data
             
         try:
-            sock.connect(('localhost',9999))#9999 is the main server
+            sock.connect((serverHOST,serverPORT))
+            
             #All sends must now have a flag before them
             #Here are codes(using 1 byte flags currently)
             #'00000000': Sending Key + Address
@@ -182,25 +211,23 @@ if __name__ == "__main__":
             sock.sendall(bytes(str(PORT),"utf-8"))
             
             #receive clientKey and add to map.
-            #Does NOT receive a Flag back from the server. Expects this data upon connection
             serverPubKey = recvBytes()
             serverHost = receive()
             serverPort = receive()
             serverData.append(serverPubKey)
             serverData.append(serverHost)
             serverData.append(serverPort)
-            #print("serverPubKey: ", serverPubKey)
             print("Connected to: ", serverHost, serverPort)
             dictionary = {}
+            
             while True:
-                #aes = AES.new(AESKey, AES.MODE_CFB, IV)
+            
                 try:
                     command = input()
                     if(len(command) > 350):
                         print("350 is the character limit. Please adjust your message. Or send multiple messages")
                         continue
-                    #print('Client Map!!: ', clientMap)
-                    #MAP is in SavedKeyBytes:["IP",NUM]
+                       
                     if command == "$exit":
                         sock.sendall(struct.pack("I",len(b'10000000')))
                         #Send the flag 10000000 for exit
@@ -208,9 +235,9 @@ if __name__ == "__main__":
                         sock.sendall(struct.pack("I",len(pubKeyInBytes)))
                         sock.sendall(pubKeyInBytes)
                         print("Ending Client.")
-                        sock.shutdown(SHUT_RDWR)
+                        sock.shutdown(socket.SHUT_RDWR)
                         sock.close()
-                        sys.exit(0)
+                        break
                     
                     elif command == "$users":#command to print number of users
                         print("Number of users connected: ", len(clientMap))
@@ -221,16 +248,13 @@ if __name__ == "__main__":
                         sock.sendall(struct.pack("I",len(pubKeyInBytes)))
                         sock.sendall(pubKeyInBytes)
                         
-                    elif command != "$show":
+                    elif command != "":
                         #Generate a packet with a path
                     
-                        #Packet looks like: [data, dst,flag,aesKey,aesIv,dst2,flag2,aeskey2,...]
                         serverAESKey = os.urandom(16)
                         serverIV = os.urandom(16)
                         encryptedData=[command, ' ', '0']
-                        #Encrypt each index of the list. 
-                        #The list acts as the packet that we are sending
-                        #It will be sent via JSON.
+                        
                         for i in range(len(encryptedData)):
                             #All of this needs to be undone by the receiver...
                             serverAES = AES.new(serverAESKey, AES.MODE_CFB, serverIV)
@@ -240,9 +264,6 @@ if __name__ == "__main__":
                             
                         enc = base64.b16encode(serverAESKey)
                         encIV = base64.b16encode(serverIV)
-                        #print("SymIV & Key: ", encIV, enc)
-                        #rsa.encrypt takes a Bytes object, and a LoadedKey.
-                        #Whoever receives this needs to undo.
                         encSKey = rsa.encrypt(enc,pubKey.load_pkcs1(serverPubKey))
                         encsIV = rsa.encrypt(encIV,pubKey.load_pkcs1(serverPubKey))
                         enc16Key = base64.b16encode(encSKey)
@@ -250,17 +271,23 @@ if __name__ == "__main__":
                         encryptedData.append(str(enc16Key,'utf-8'))
                         encryptedData.append(str(enc16IV,'utf-8'))
                         
-                        #print("EncryptedData: ",encryptedData, len(encryptedData))
                         firstDestination=str(serverPubKey,'utf-8')
-                        #print("Packet Constructed")
+                        
                         previousKey = str(serverPubKey,'utf-8')
                         
                         #Now encrypting by each path
-                        for key in clientMap:
-                            #print("Compare: ",key, pubKeyInBytes)
+                        counter = 0
+                        
+                        allKeys = list(clientMap.keys())
+                        random.shuffle(allKeys)
+                        for key in allKeys:
+
                             if bytes(key,'utf-8') == pubKeyInBytes:
-                                #print("Keys are the same, skip")
                                 continue
+                                
+                            elif counter == 5:
+                                break
+                                
                             else:
                                 AESKey = os.urandom(16)
                                 IV = os.urandom(16)
@@ -271,14 +298,12 @@ if __name__ == "__main__":
                                     newData = aes.encrypt(encryptedData[i])
                                     newData = base64.b16encode(newData)
                                     encryptedData[i] = str(newData,'utf-8')
-                                encodedKey = base64.b16encode(AESKey) #this is informat b'xxxx'
+                                encodedKey = base64.b16encode(AESKey)
                                 encodedIV = base64.b16encode(IV)
-                                #print("16Key & IV: ", encodedKey, encodedIV)
-                                encryptedKey = rsa.encrypt(encodedKey, pubKey.load_pkcs1(bytes(key,'utf-8')))#convert to bytes and decode before using
+                                encryptedKey = rsa.encrypt(encodedKey, pubKey.load_pkcs1(bytes(key,'utf-8')))
                                 encryptedIV = rsa.encrypt(encodedIV, pubKey.load_pkcs1(bytes(key,'utf-8')))
                                 encrypted16Key = base64.b16encode(encryptedKey)
-                                encrypted16IV = base64.b16encode(encryptedIV)
-                                
+                                encrypted16IV = base64.b16encode(encryptedIV)                                
 
                                 encryptedData.append(str(encrypted16Key,'utf-8'))
                                 encryptedData.append(str(encrypted16IV,'utf-8'))
@@ -286,46 +311,53 @@ if __name__ == "__main__":
                                 previousKey = key
                                 
                                 firstDestination = key
-                    
-                        
                                 
+                                counter += 1
+                                                                            
                         #Send packet to server
-                        #print(str(serverHost), serverPort)
                         jsonData = json.dumps(encryptedData)
-                        #print("Connecting to: ", clientMap[firstDestination])
                         aSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
                         if(firstDestination) == str(serverPubKey,'utf-8'):
                             aSock.connect((serverHost,int(serverPort)))
                         else:
                             aSock.connect((clientMap[firstDestination][0],int(clientMap[firstDestination][1])))
-                        aSock.sendall(struct.pack("I",len(b'00000001')))
+
                         #Send the flag 00000001
                         #This flag indicates that the receiver needs to decrypt the message
+                        aSock.sendall(struct.pack("I",len(b'00000001')))
                         aSock.sendall(b'00000001')
-                        aSock.sendall(struct.pack("I",len(bytes(jsonData,'utf-8'))))
-                        aSock.sendall(bytes(jsonData,'utf-8'))
-                        aSock.shutdown(SHUT_RDWR)
+                        #aSock.sendall(struct.pack("I",len(bytes(jsonData,'utf-8'))))
+                        #aSock.sendall(bytes(jsonData,'utf-8'))
+                        #print(jsonData)
+                        segments = []
+                        currString = ''
+                        for i in range(len(jsonData)):
+                            currString += jsonData[i]
+                            if((i % 4000 == 0) and i>0):
+                                segments.append(currString)
+                                currString = ''
+                        segments.append(currString)
+                        #print('jsonData ',jsonData)
+                        #print('segments: ',segments)
+                        numSegments = len(segments)
+                        aSock.sendall(struct.pack("I",1))
+                        aSock.sendall(bytes(str(numSegments),'utf-8'))
+                        for seg in segments:
+                            aSock.sendall(struct.pack("I",len(seg)))
+                            aSock.sendall(bytes(seg,'utf-8'))
+                        aSock.shutdown(socket.SHUT_RDWR)
                         aSock.close()
                         
-                       
-                        
-                        #command = rsa.encrypt(bytes(command,"utf-8"), pubKey.load_pkcs1(serverPubKey))
-                        #command = aes.encrypt(command)
-                        #sock.sendall(bytes(command,"utf-8"))          
-                        #size = len(command)
-                        #size32bit = struct.pack("I",size)
-                        #sock.sendall(size32bit)#sendalling the size of the command
-                        #sock.sendall(command)#command to sendall
-                    
                 except:
                     sock.close()
                     print("ERROR: Something unexpected happened while sending packet", file=sys.stderr)
                     sys.exit(0)
         except:
-            #print("ERROR: Could not connect to server. Terminating.", file=sys.stderr)
+            print("ERROR: Could not connect to server. Terminating.", file=sys.stderr)
             sys.exit(0)    
             
         finally:
+            sock.close()
             print("End Client")
     
     server = ThreadedTCPServer((HOST,PORT), MyTCPHandler)
