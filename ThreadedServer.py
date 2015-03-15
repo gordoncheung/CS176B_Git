2f31a28820afa27dd5b492ee78744a310e739fcf
gordoncheung <itsagordon@gmail.com>
2015-03-15 15:17:26 -0700

    Update ThreadedServer.py

--- ThreadedServer.py
+++ ThreadedServer.py
@@ -131,7 +131,6 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                         #Broadcast the message to all clients
                         for client in clientMap:
                             sockt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                            print(clientMap[client])
                             sockt.connect(clientMap[client])
                             sockt.sendall(struct.pack("I",len(b'00000005')))#Flag 00000005 to notify client to print msg
                             sockt.sendall(b'00000005')
@@ -187,7 +186,8 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("ERROR: Invalid number of args. Terminating.")
         sys.exit(0)
-    HOST, PORT = "169.231.37.36",  int(sys.argv[1])
+    hostname = socket.gethostname()
+    HOST, PORT = socket.gethostbyname(hostname),  int(sys.argv[1])
     if (PORT > 65535 or PORT < 1024):
         print("ERROR: Invalid port. Terminating.", file=sys.stderr)
         sys.exit(0)
@@ -198,7 +198,8 @@ if __name__ == "__main__":
     postNumber = 0
     
     try:
-        server = ThreadedTCPServer((HOST,9999), MyTCPHandler)#9999 is main port for now
+        print("Hosting on: ", HOST, PORT)
+        server = ThreadedTCPServer((HOST,int(sys.argv[1])), MyTCPHandler)#9999 is main port for now
         HOST, PORT = server.server_address
         server.allow_reuse_address=True
         server.serve_forever()
